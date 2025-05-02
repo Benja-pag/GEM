1c383ded6b550211edf7d357e17b7ea35a3be572
Bhiestt <je.santis@duocuc.cl>
2025-05-01 22:45:19 -0400

    olvide mi contraseña
    avance de olvide mi contraseña

--- GEM/settings.py
+++ GEM/settings.py
@@ -131,3 +131,12 @@ DATATABLES = {
         'url': '//cdn.datatables.net/plug-ins/1.13.7/i18n/es-ES.json'
     }
 }
+
+# Configuración de correo electrónico
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', '')
--- GEM/urls.py
+++ GEM/urls.py
@@ -23,4 +23,8 @@ urlpatterns = [
     path('', include('Core.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
